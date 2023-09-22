8a86bde19e0d4928170522ec03fa2432752abae6
sailendra131 <sailendra.n.jena@gmail.com>
2023-09-23 02:36:00 +0530

    First Commit

---
+++ src/main/java/com/jenkin/docker/api/SpringJenkinDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jenkin.docker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinDockerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkin/docker/api/SpringJenkinDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkin.docker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

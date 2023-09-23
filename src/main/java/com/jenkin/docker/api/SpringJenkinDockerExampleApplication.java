4d2c8c3d4a0e06b42ece12e93671e25a2573e8da
sailendra131 <sailendra.n.jena@gmail.com>
2023-09-24 00:46:57 +0530

    First Commit from Spring Boot Application

--- src/main/java/com/jenkin/docker/api/SpringJenkinDockerExampleApplication.java
+++ src/main/java/com/jenkin/docker/api/SpringJenkinDockerExampleApplication.java
@@ -1,12 +1,23 @@
 package com.jenkin.docker.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringJenkinDockerExampleApplication {
+	public static Logger log = LoggerFactory.getLogger(SpringJenkinDockerExampleApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		log.info("This is first Commit from Spring Boot Application.....");
+	}
 
 	public static void main(String[] args) {
+		log.info("This is first execution from the Spring Boot Application.....");
 		SpringApplication.run(SpringJenkinDockerExampleApplication.class, args);
 	}
 
